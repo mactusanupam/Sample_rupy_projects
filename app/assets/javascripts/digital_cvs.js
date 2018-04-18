b3f338afc53baa728447661e8a81f9f3be6d4090
Vikram Mishra <vmishra@godaddy.com>
2018-04-18 15:05:23 +0530

    character counter added

--- app/assets/javascripts/digital_cvs.js
+++ app/assets/javascripts/digital_cvs.js
@@ -15,13 +15,27 @@ $(document).ready(function(){
     $('.show-cv-name').show();
   });
 
+  //Fix Alignment for date fields and labels
   $('.digital_cv_honor_and_awards_award_date .form-inline, .digital_cv_certifications_date .form-inline, .personal_detail_date_of_birth .form-inline').removeClass('col-sm-12 no-padding').addClass('col-sm-9');
   $('.edit_contact_detail .control-label').removeClass('col-sm-2');
 
+  //end date disable enable for experience and education
   $(".resume-editor").on("change", 'input[type=checkbox]', function(e){
     var endDate = $(this).parents('.checkbox').parents('.form-group').prev('div.col-sm-4');
     endDate.find('input, select').prop('disabled', function(i, v) { return !v; });
     endDate.toggle();
+  });
+
+  //textarea character counter
+  $('.digital-cv-form').on('keyup', '.digital_cv_summary textarea, .digital_cv_objective textarea', function(){
+    var $this = $(this);
+    var len = $this.val().length;
+
+    if (len >= 500) {
+      $this.value = $this.value.substring(0, 500);
+    } else {
+      $this.parents('.text').next('.help-block').find('.char-count').text(500 - len);
+    }
   });
 
   //Show New company form
