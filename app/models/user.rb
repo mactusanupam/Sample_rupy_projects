11cc417756a54deec060bd7745676c99d71cdb49
Vikram Mishra <vmishra@godaddy.com>
2018-05-10 13:29:17 +0530

    Fixed conditional issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,18 @@ class User < ApplicationRecord
   belongs_to :user_type
   belongs_to :company, optional: true
   validates :first_name, :last_name, presence: true
-  
+
   has_many :jobs
   has_many :job_applications, dependent: :destroy
 
   accepts_nested_attributes_for :company, :allow_destroy => true
 
   def admin?
-    self.user_type.title = 'Admin'
+    self.user_type.title == 'Admin'
   end
 
   def super_admin?
-    self.user_type.title = 'Super Admin'
+    self.user_type.title == 'Super Admin'
   end
 
   def has_applied?(job_id)
@@ -27,10 +27,10 @@ class User < ApplicationRecord
   end
 
   def employer?
-    self.user_type.title = 'Employer'
+    self.user_type.title == 'Employer'
   end
 
   def individual?
-    self.user_type.title = 'Individual'
+    self.user_type.title == 'Individual'
   end
 end
